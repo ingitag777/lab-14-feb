988fee25578f694e3711f2c56cd4f7e28a33ebf1
Ingit <ingitag777@gmail.com>
2019-02-14 10:23:06 +0530

    first repo

---
+++ a1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter the value of a and b\n";
+	cin>>a>>b;
+        sum=a+b;
+	cout<<"Sum of a and b  "<<sum;
+	return 0;
+}
+
