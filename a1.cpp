72b003bc4e3eec7a778cda219f1527c1c030c9c8
ingitag777 <40909169+ingitag777@users.noreply.github.com>
2019-02-14 10:56:56 +0530

    Update a1.cpp

--- a1.cpp
+++ a1.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
 	int a,b,sum;
-	cout<<"Enter the value of a and b\n";
+	cout<<"Enter the value of a and b\n";// asking the value for a,b
 	cin>>a>>b;
         sum=a+b;
-	cout<<"Sum of a and b  "<<sum;
+	cout<<"Sum of a and b  "<<sum; // printing the sum
 	return 0;
 }
 
